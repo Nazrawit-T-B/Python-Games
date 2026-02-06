d54bb736cb37ef77ceae019a4b36fdc39df06896
Nazrawit-T-B <nazrawitbezabih@gmail.com>
2026-02-06 16:05:30 +0300

    draw the couple of colors in the screen in random places

--- ColorPuzzle.py
+++ ColorPuzzle.py
@@ -5,10 +5,10 @@ pygame.init()
 #Constants defining the width and height of parameters
 Wwidth=640
 Wheight=480
-Box=40
+Box=60
 Gap=10
-NBoxesHorizontally=10
-NBoxesVertically=7
+NBoxesHorizontally=4
+NBoxesVertically=4
 Xmargin=int((Wwidth-(NBoxesHorizontally*(Box+Gap)))/2)
 Ymargin=int((Wheight-(NBoxesVertically*(Box+Gap)))/2)
 
@@ -24,11 +24,33 @@ BoxColor=WHITE
 HighLightC=BLUE
 
 def generateColors():
-    return ((random.randint(0,255),random.randint(0,255),random.rantint(0,255)))
+    return ((random.randint(0,255),random.randint(0,255),random.randint(0,255)))
 
+
+def coloredSquares(screen):
+    items=[]
+    for i in range(5):
+        for j in range(5):
+            items.append(generateColors())
+    random.shuffle(items)
+    num=int(NBoxesHorizontally*NBoxesVertically/2)
+    items=items[:num]*2
+    random.shuffle(items)
+    index = 0 
+    for i in range(NBoxesHorizontally): 
+        for j in range(NBoxesVertically): 
+            x = Xmargin + i*(Box+Gap) 
+            y = Ymargin + j*(Box+Gap) 
+            rect = pygame.Rect(x, y, Box, Box) 
+            pygame.draw.rect(screen, items[index], rect) 
+            index += 1
+    return 
+#main game logic
 def main():
     display=pygame.display.set_mode((Wwidth,Wheight))
-    display.fill(BgColor)
+    pygame.display.set_caption('Color Match')
+    display.fill(LightBg)
+    coloredSquares(display)
     while True:
         for event in pygame.event.get():
             if event.type==QUIT:
