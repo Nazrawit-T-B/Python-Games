6209a526faf8dc50cf27c272b916a30e9cbd7d36
Nazrawit-T-B <nazrawitbezabih@gmail.com>
2026-02-09 23:35:28 +0300

    layed basic structure

--- RatsAndCheese/Rats.py
+++ RatsAndCheese/Rats.py
@@ -1,16 +1,19 @@
 import pygame,sys,random
 from pygame.locals import*
 pygame.init()
-backg=pygame.image.load('RatsAndCheese/background.png')
+backg=pygame.image.load('RatsAndCheese/image.png')
+icon=pygame.image.load('RatsAndCheese/icons.png')
 def main():
-    display=pygame.display.set_mode((640,480))
+    display=pygame.display.set_mode((730,600))
     pygame.display.set_caption('Rats and Cheese')
-    display.blit(backg,(0,0))
+    pygame.display.set_icon(icon)
+    
     while True:
         for event in pygame.event.get():
             if event.type==QUIT:
                 pygame.quit()
                 sys.exit()
+        display.blit(backg,(0,0))
         pygame.display.update()
 if __name__=='__main__':
     main()
